0616189260956da2ec9b837e717d09b4ede3e796
Derek Lao <dlao00@stuy.edu>
2018-12-13 10:11:11 -0500

    added class for Node and MyLinkedList

---
+++ MyLinkedList.java
@@ -0,0 +1,9 @@
+public class MyLinkedList{
+  private Node start,end;
+  private int length;
+
+  public MyLinkedList(){
+    length=0;
+  }
+
+}
---
+++ Node.java
@@ -0,0 +1,11 @@
+public class Node{
+  // we want the user to see the DATA in the node, but NOT THE NODE ITSELF
+  private Node next,prev;
+  private Integer data;
+
+  public Node next(){}
+  public Node prev(){}
+  public void setNext(Node other){}
+  public void setPrev(Node other){}
+  public String toString(){}
+}
