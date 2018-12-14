d9849a67d108c1fbd7f312f74a99430215cabd5d
Derek Lao <dlao00@stuy.edu>
2018-12-14 09:39:18 -0500

    added method shells to MyLinkedList

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -6,4 +6,17 @@ public class MyLinkedList{
     length=0;
   }
 
+  public boolean add(Integer value){}
+  public int size(){}
+  //note you don't have get(index) yet, nor would you want to use it here
+  public String toString(){}
+  public Integer get(int index)
+  public Integer set(int index,Integer value)
+
+  public boolean contains(Integer value)
+  public int indexOf(Integer value)
+
+  public void add(int index,Integer value)
+  public Integer remove(int index)
+  public boolean remove(Integer value) //indexOf() would also be useful
 }
