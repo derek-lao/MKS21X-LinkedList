da8f25e804569fc37f8551ad3e03b3270594ae8e
Derek Lao <dlao00@stuy.edu>
2018-12-16 17:35:21 -0500

    wrote indexOf, changed code completely in contains to use code from indexOf

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -10,7 +10,6 @@ public class MyLinkedList{
   }
 
   /** adds the specified element to the end of the list
-  *
   * @param value is the specified elmeent being added
   * I will attempt to change the end node to a different node,
   * with data as the element
@@ -21,11 +20,18 @@ public class MyLinkedList{
     length++;
     return true;
   }
+
+  /**
+  *@return the number of elements in this list
+  */
   public int size(){
     return length;
   }
 
-  //note you don't have get(index) yet, nor would you want to use it here
+  /**
+  *note you don't have get(index) yet, nor would you want to use it here
+  *@return a string in the form of [element,element,element]
+  */
   public String toString(){
     String answer="["+start.get();
     for(Node current=start;current.hasNext();current=current.next())
@@ -34,12 +40,22 @@ public class MyLinkedList{
     }
     return answer+"]";
   }
+
+  /**
+  *Returns the element at the specified position in this list.
+  *@param index is the specified position
+  *@return the element at that specified position
+  */
   public Integer get(int index){
     return this.getNode(index).get();
   }
 
   /**
-  * @return the element previously at the specified positions
+  *replaces element at the specified position on the list with
+  a specified value, and returns the previous value
+  *@param index is the specified position
+  *@param value is the element that is to replace the original value
+  *@return the element previously at the specified positions
   */
   public Integer set(int index,Integer value){
     Integer originalValue=this.getNode(index).get();
@@ -57,16 +73,37 @@ public class MyLinkedList{
   // }
   */
 
+  /**
+  *check if the value is contained in the list at all
+  @param value is the value that the user is going to check the whole list for
+  @return true if the value is found, and false if the value has not been found
+  */
   public boolean contains(Integer value){
-    for(Node current=start;current.hasNext();current=current.next())
+    //we might use indexOf
+    return indexOf(value)!=-1;
+  }
+
+  /**
+  *Returns the index of the first occurrence of the specified element
+  in this list, or -1 if this list does not contain the element
+  *@param value is the specified elements
+  *@return the index of the first occurence, or -1 if the element
+  is not contained
+  */
+  public int indexOf(Integer value){
+    Node current=start;
+    int i=0;
+    while(current.hasNext())
     {
       if(current.get()==value)
-      return true;
+      {
+        return i;
+      }
+      current=current.next();
+      i++;
     }
-    return false;
+    return -1;
   }
-  public int indexOf(Integer value){}
-
   public void add(int index,Integer value){}
   public Integer remove(int index){}
   public boolean remove(Integer value){} //indexOf() would also be useful
@@ -77,6 +114,7 @@ public class MyLinkedList{
     this.start.setPrev(null);
     this.end.setNext(null);
   }
+
   // helper function to loop through the linked list to get a node
   // at a specific index.  That node would be used for whatever it needs
   // to be used for in other methods, such as get(index) and set(value,index)
