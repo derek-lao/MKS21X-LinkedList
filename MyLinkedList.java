1a556261a0998cd539092c5afe83a79561ca6a6e
Derek Lao <dlao00@stuy.edu>
2018-12-16 14:23:41 -0500

    added things for add() and size(), added constructor for Node to make it easier to work with

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,4 +1,5 @@
 public class MyLinkedList{
+  // we want the user to see the DATA in the node, but NOT THE NODE ITSELF
   private Node start,end;
   private int length;
 
@@ -6,17 +7,29 @@ public class MyLinkedList{
     length=0;
   }
 
-  public boolean add(Integer value){}
-  public int size(){}
+  /** adds the specified element to the end of the list
+  *
+  * @param value is the specified elmeent being added
+  * I will attempt to change the end node to a different node,
+  * with data as the element
+  */
+  public boolean add(Integer value){
+    Node answer=new Node(value);
+    end.setNext(answer);
+    return true;
+  }
+  public int size(){
+    return length;
+  }
   //note you don't have get(index) yet, nor would you want to use it here
   public String toString(){}
-  public Integer get(int index)
-  public Integer set(int index,Integer value)
+  public Integer get(int index){}
+  public Integer set(int index,Integer value){}
 
-  public boolean contains(Integer value)
-  public int indexOf(Integer value)
+  public boolean contains(Integer value){}
+  public int indexOf(Integer value){}
 
-  public void add(int index,Integer value)
-  public Integer remove(int index)
-  public boolean remove(Integer value) //indexOf() would also be useful
+  public void add(int index,Integer value){}
+  public Integer remove(int index){}
+  public boolean remove(Integer value){} //indexOf() would also be useful
 }
--- Node.java
+++ Node.java
@@ -1,11 +1,23 @@
 public class Node{
-  // we want the user to see the DATA in the node, but NOT THE NODE ITSELF
+
   private Node next,prev;
   private Integer data;
 
-  public Node next(){}
-  public Node prev(){}
-  public void setNext(Node other){}
-  public void setPrev(Node other){}
+  public Node(Integer element){
+    data=element;
+  }
+  
+  public Node next(){
+    return this.next;
+  }
+  public Node prev(){
+    return this.prev;
+  }
+  public void setNext(Node other){
+    this.next=other;
+  }
+  public void setPrev(Node other){
+    this.prev=other;
+  }
   public String toString(){}
 }
